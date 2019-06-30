0bb820d99297ab2f20d3bebd34120ac24318a241
Fabizio <fabrizio.cabaleiro@gmail.com>
2019-06-30 20:08:22 +0100

    Adding new headers

---
+++ dht11.h
@@ -0,0 +1,5 @@
+/* vim: set foldmethod=marker: */
+#ifndef _DHT11_H_
+#define _DHT11_H_
+#define DHT11_PAYLOAD_LEN                    5 /* 40 Bits */
+#endif
---
+++ timer.h
@@ -0,0 +1,9 @@
+/* vim: set foldmethod=marker: */
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+/* Timer counter for seconds, minutes, hours */
+#define TIMER1_SECONDS_LEN             4
+#define TIMER1_EVENTS_SECOND           0
+
+#endif
