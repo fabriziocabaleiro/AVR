b5a5d8173498368f8c5d671b5bba0d3bc252e4ca
Fabrizio Cabaleiro <fabrizio.cabaleiro@gmail.com>
2019-11-24 10:26:53 +0000

    UTILS: adding sdiv (single division)
    TCP: http response working, FIN reply working as well
    
    IPV4: Internet checksum had limitation of even package size, now odd
    package size supported as well.

--- dht11.h
+++ dht11.h
@@ -2,4 +2,11 @@
 #ifndef _DHT11_H_
 #define _DHT11_H_
 #define DHT11_PAYLOAD_LEN                    5 /* 40 Bits */
+
+/* Payload offsets */
+#define DHT11_DATA_RH_INT  0 /* Relative humidity integer part */
+#define DHT11_DATA_RH_FRAC 1 /* Relative humidity fractional part */
+#define DHT11_DATA_T_INT   2 /* Temperature integer part */
+#define DHT11_DATA_T_FRAC  3 /* Temperature fractional part */
+#define DHT11_DATA_CHKSUM  4 /* Checksum */
 #endif
